51be605d66ceb3e6813aa99134f90c8343af599b
samkum002 <sameerkumarkumar220705@gmail.com>
2026-07-12 12:07:17 +0530

    Initial commit

---
+++ src/main/java/com/dev/ResQNet/ResQNetApplication.java
@@ -0,0 +1,13 @@
+package com.dev.ResQNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResQNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResQNetApplication.class, args);
+	}
+
+}
