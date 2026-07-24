327353c16822323d1cfa64fbbfa877a16ed6971f
samkum002 <sameerkumarkumar220705@gmail.com>
2026-07-24 14:31:34 +0530

    SpringAI Configuration - I

--- src/main/java/com/dev/ResQNet/ResQNetApplication.java
+++ src/main/java/com/dev/ResQNet/ResQNetApplication.java
@@ -3,7 +3,11 @@ package com.dev.ResQNet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ResQNetApplication {
 
